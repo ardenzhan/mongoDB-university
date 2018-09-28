b45669a442d9bf6069cb0d0706fc20eb4be91496
Arden Zhan <arden@protonmail.ch>
2018-09-28 16:18:15 -0500

    Week 3 - The Node.js Driver handouts

---
+++ m101js/3-nodejs-driver/queryOperatorsInNodeJSDriver/app.js
@@ -0,0 +1,82 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const options = commandLineOptions();
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'crunchbase';
+
+MongoClient.connect(url, (err, client) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  const query = queryDocument(options);
+  const projection = {"_id": 1, "name": 1, "founded_year": 1,
+                      "number_of_employees": 1, "crunchbase_url": 1};
+
+  let cursor = db.collection('companies').find(query);
+  cursor.project(projection);
+
+  let numMatches = 0;
+
+  cursor.forEach(
+    doc => {
+      numMatches += 1;
+      console.log(doc);
+    },
+    err => {
+      assert.equal(err, null);
+      console.log("Our query was: " + JSON.stringify(query));
+      console.log("Matching documents: " + numMatches);
+      return db.close();
+    }
+  );
+});
+
+function queryDocument(options) {
+  console.log(options);
+  let query = {
+    "founded_year": {
+      "$gte": options.firstYear,
+      "$lte": options.lastYear
+    }
+  };
+  if (options.employees) {
+    query.number_of_employees = { "$gte": options.employees };
+  }
+  return query;
+}
+
+function commandLineOptions() {
+  const optionDefinitions = [
+    { name: "firstYear", alias: "f", type: Number },
+    { name: "lastYear", alias: "l", type: Number },
+    { name: "employees", alias: "e", type: Number }
+  ];
+  const options = commandLineArgs(optionDefinitions);
+
+  const sections = [
+    {
+      header: 'Usage',
+      content: 'The first two options below are required. The rest are optional.'
+    },
+    {
+      header: 'Options',
+      optionList: optionDefinitions
+    }
+  ];
+  const usage = commandLineUsage(sections);
+
+  // if (!(options.firstYear && options.lastYear)) {
+  // if (!(options.hasOwnProperty("firstYear") && options.hasOwnProperty("lastYear"))) {
+  if (!("firstYear" in options && "lastYear" in options)) {
+    console.log(usage);
+    process.exit();
+  }
+
+  return options;
+}
