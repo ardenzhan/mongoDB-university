16657cb3dba803a768bb13d4920e48ab207cbe33
Arden Zhan <arden@protonmail.ch>
2018-10-16 22:04:15 -0500

    lab 6 and 7

--- m101js/final/cart.js
+++ m101js/final/cart.js
@@ -22,37 +22,43 @@ class CartDAO {
   }
 
 
+  /*
+   * TODO LAB #6
+   *
+   * Write a query that will determine whether or not the cart associated
+   * with the userId contains an item identified by itemId. If the cart
+   * does contain the item, pass the item to the callback. If it does not,
+   * pass the value null to the callback.
+   *
+   * NOTE: You should pass only the matching item to the callback. Do not
+   * pass an array of one or more items or the entire cart.
+   *
+   * SUGGESTION: While it is not necessary, you might find it easier to
+   * use the $ operator in a projection document in your call to find() as
+   * a means of selecting the matching item. Again, take care to pass only
+   * the matching item (not an array) to the callback. See:
+   * https://docs.mongodb.org/manual/reference/operator/projection/positional/
+   *
+   * As context for this method to better understand its purpose, look at
+   * how cart.itemInCart is used in the mongomart.js app.
+   */
   itemInCart(userId, itemId, callback) {
-
-        /*
-         *
-         * TODO-lab6
-         *
-         * LAB: #6
-         *
-         * Write a query that will determine whether or not the cart associated
-         * with the userId contains an item identified by itemId. If the cart
-         * does contain the item, pass the item to the callback. If it does not,
-         * pass the value null to the callback.
-         *
-         * NOTE: You should pass only the matching item to the callback. Do not
-         * pass an array of one or more items or the entire cart.
-         *
-         * SUGGESTION: While it is not necessary, you might find it easier to
-         * use the $ operator in a projection document in your call to find() as
-         * a means of selecting the matching item. Again, take care to pass only
-         * the matching item (not an array) to the callback. See:
-         * https://docs.mongodb.org/manual/reference/operator/projection/positional/
-         *
-         * As context for this method to better understand its purpose, look at
-         * how cart.itemInCart is used in the mongomart.js app.
-         *
-         */
-
-        callback(null);
-
-        // TODO-lab6 Replace all code above (in this method).
-    }
+    this.db.collection('cart')
+    .find(
+      { userId: userId, "items._id": itemId },
+      { "items.$": 1 }
+    )
+    .limit(1)
+    .next((err, item) => {
+      assert.equal(err, null);
+      console.log(err);
+      if (item != null) {
+        item = item.items[0];
+      }
+      console.log(item);
+      callback(item);
+    });
+  }
 
 
     /*
@@ -120,57 +126,40 @@ class CartDAO {
     };
 
 
+  /*
+  * TODO LAB #7: Update the quantity of an item in the user's cart in the
+  * database by setting quantity to the value passed in the quantity
+  * parameter. If the value passed for quantity is 0, remove the item
+  * from the user's cart stored in the database.
+  *
+  * Pass the updated user's cart to the callback.
+  *
+  * NOTE: Use the solution for addItem as a guide to your solution for
+  * this problem. There are several ways to solve this. By far, the
+  * easiest is to use the $ operator. See:
+  * https://docs.mongodb.org/manual/reference/operator/update/positional/
+  *
+  */
   updateQuantity(userId, itemId, quantity, callback) {
+    let updateDoc = {};
 
-        /*
-        * TODO-lab7
-        *
-        * LAB #7: Update the quantity of an item in the user's cart in the
-        * database by setting quantity to the value passed in the quantity
-        * parameter. If the value passed for quantity is 0, remove the item
-        * from the user's cart stored in the database.
-        *
-        * Pass the updated user's cart to the callback.
-        *
-        * NOTE: Use the solution for addItem as a guide to your solution for
-        * this problem. There are several ways to solve this. By far, the
-        * easiest is to use the $ operator. See:
-        * https://docs.mongodb.org/manual/reference/operator/update/positional/
-        *
-        */
-
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        dummyItem.quantity = quantity;
-        userCart.items.push(dummyItem);
-        callback(userCart);
-
-        // TODO-lab7 Replace all code above (in this method).
-
+    if (quantity == 0) {
+      updateDoc = { "$pull": { items: { _id: itemId } } };
+    } else {
+      updateDoc = { "$set": { "items.$.quantity": quantity } };
     }
 
-  createDummyItem() {
-
-        var item = {
-            _id: 1,
-            title: "Gray Hooded Sweatshirt",
-            description: "The top hooded sweatshirt we offer",
-            slogan: "Made of 100% cotton",
-            stars: 0,
-            category: "Apparel",
-            img_url: "/img/products/hoodie.jpg",
-            price: 29.99,
-            quantity: 1,
-            reviews: []
-        };
-
-        return item;
-    }
-
+    this.db.collection('cart').findOneAndUpdate(
+      { userId: userId,
+        "items._id": itemId },
+      updateDoc,
+      { returnOriginal: false },
+      (err, result) => {
+        assert.equal(err, null);
+        console.log(result.value);
+        callback(result.value);
+      });
+  }
 }
-
 
 module.exports.CartDAO = CartDAO;
