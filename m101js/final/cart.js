9e3e1ccd94b158905452cb3753535cfc9b1fd108
Arden Zhan <arden@protonmail.ch>
2018-10-16 21:14:31 -0500

    lab 5, updated es6 class syntax

--- m101js/final/cart.js
+++ m101js/final/cart.js
@@ -1,61 +1,28 @@
-/*
-  Copyright (c) 2008 - 2016 MongoDB, Inc. <http://mongodb.com>
+const assert = require('assert');
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-
-
-function CartDAO(database) {
-    "use strict";
-
+class CartDAO {
+  constructor(database) {
     this.db = database;
+  }
+
+  /*
+  * TODO LAB #5: Implement the getCart() method.
+  *
+  * Query the "cart" collection by userId and pass the cart to the
+  * callback function.
+  */
+  getCart(userId, callback) {
+    this.db.collection('cart').find({ userId: userId })
+    .limit(1)
+    .next((err, userCart) => {
+      assert.equal(err, null);
+      assert.ok(userCart);
+      callback(userCart);
+    });
+  }
 
 
-    this.getCart = function(userId, callback) {
-        "use strict";
-
-        /*
-        * TODO-lab5
-        *
-        * LAB #5: Implement the getCart() method.
-        *
-        * Query the "cart" collection by userId and pass the cart to the
-        * callback function.
-        *
-        */
-
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-
-        // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
-        callback(userCart);
-    }
-
-
-    this.itemInCart = function(userId, itemId, callback) {
-        "use strict";
+  itemInCart(userId, itemId, callback) {
 
         /*
          *
@@ -106,8 +73,7 @@ function CartDAO(database) {
      * http://mongodb.github.io/node-mongodb-native/2.0/api/Collection.html#findOneAndUpdate
      *
      */
-    this.addItem = function(userId, item, callback) {
-        "use strict";
+  addItem(userId, item, callback) {
 
         // Will update the first document found matching the query document.
         this.db.collection("cart").findOneAndUpdate(
@@ -154,8 +120,7 @@ function CartDAO(database) {
     };
 
 
-    this.updateQuantity = function(userId, itemId, quantity, callback) {
-        "use strict";
+  updateQuantity(userId, itemId, quantity, callback) {
 
         /*
         * TODO-lab7
@@ -187,8 +152,7 @@ function CartDAO(database) {
 
     }
 
-    this.createDummyItem = function() {
-        "use strict";
+  createDummyItem() {
 
         var item = {
             _id: 1,
