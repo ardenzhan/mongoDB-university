e7baf7022820a52ea3e8ba854a64770bd43b2d8b
Arden Zhan <arden@protonmail.ch>
2018-10-16 20:20:43 -0500

    lab 2, updated to es6 and 3.x driver syntax

--- m101js/final/README.md
+++ m101js/final/README.md
@@ -36,3 +36,15 @@ Select the "Apparel" category by clicking on it with your mouse. This category c
  - [x] WiredTiger T-shirt
  - [ ] Green T-shirt
  - [ ] MongoDB University T-shirt
+
+## Lab 2
+```
+db.item.createIndex({ title: "text", slogan: "text", description: "text" })
+```
+In this lab, you will implement the methods in items.js necessary to support the route for text search or "/search". This route is implemented in mongomart.js in the function that begins with this line:
+```
+router.get("/search", function(req, res) {
+```
+The methods you will implement in items.js are: searchItems(), and getNumSearchItems(). The comments in each of these methods describe in detail what you need to do to implement each method. When you are finished, restart the mongomart.js application and answer the question below.
+
+How many products match the query, "leaf"? **7**
--- m101js/final/items.js
+++ m101js/final/items.js
@@ -106,67 +106,68 @@ class ItemDAO {
   }
 
 
+  /*
+   * TODO LAB #2A: Implement searchItems()
+   *
+   * Using the value of the query parameter passed to searchItems(),
+   * perform a text search against the "item" collection.
+   *
+   * Sort the results in ascending order based on the _id field.
+   *
+   * Select only the items that should be displayed for a particular
+   * page. For example, on the first page, only the first itemsPerPage
+   * matching the query should be displayed.
+   *
+   * Use limit() and skip() and the method parameters: page and
+   * itemsPerPage to select the appropriate matching products. Pass these
+   * items to the callback function.
+   *
+   * searchItems() depends on a text index. Before implementing
+   * this method, create a SINGLE text index on title, slogan, and
+   * description. You should simply do this in the mongo shell.
+   */
   searchItems(query, page, itemsPerPage, callback) {
-        /*
-         * TODO-lab2A
-         *
-         * LAB #2A: Implement searchItems()
-         *
-         * Using the value of the query parameter passed to searchItems(),
-         * perform a text search against the "item" collection.
-         *
-         * Sort the results in ascending order based on the _id field.
-         *
-         * Select only the items that should be displayed for a particular
-         * page. For example, on the first page, only the first itemsPerPage
-         * matching the query should be displayed.
-         *
-         * Use limit() and skip() and the method parameters: page and
-         * itemsPerPage to select the appropriate matching products. Pass these
-         * items to the callback function.
-         *
-         * searchItems() depends on a text index. Before implementing
-         * this method, create a SINGLE text index on title, slogan, and
-         * description. You should simply do this in the mongo shell.
-         *
-         */
-
-        var item = this.createDummyItem();
-        var items = [];
-        for (var i=0; i<5; i++) {
-            items.push(item);
-        }
-
-        // TODO-lab2A Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the items for the selected page
-        // of search results to the callback.
-        callback(items);
+    let queryDoc;
+    if (query.trim() == "") {
+      queryDoc = {};
+    } else {
+      queryDoc = { "$text": { "$search": query } };
     }
 
+    let cursor = this.db.collection('item').find(queryDoc);
+    cursor.sort({ _id: 1 });
+    cursor.skip(page * itemsPerPage);
+    cursor.limit(itemsPerPage);
+    cursor.toArray((err, items) => {
+      assert.equal(err, null);
+      callback(items);
+    });
+  }
 
+
+  /*
+  * TODO LAB #2B: Using the value of the query parameter passed to this
+  * method, count the number of items in the "item" collection matching
+  * a text search. Pass the count to the callback function.
+  *
+  * getNumSearchItems() depends on the same text index as searchItems().
+  * Before implementing this method, ensure that you've already created
+  * a SINGLE text index on title, slogan, and description. You should
+  * simply do this in the mongo shell.
+  */
   getNumSearchItems(query, callback) {
-    // this.getNumSearchItems = function(query, callback) {
-    //     "use strict";
-
-        var numItems = 0;
-
-        /*
-        * TODO-lab2B
-        *
-        * LAB #2B: Using the value of the query parameter passed to this
-        * method, count the number of items in the "item" collection matching
-        * a text search. Pass the count to the callback function.
-        *
-        * getNumSearchItems() depends on the same text index as searchItems().
-        * Before implementing this method, ensure that you've already created
-        * a SINGLE text index on title, slogan, and description. You should
-        * simply do this in the mongo shell.
-        */
-
-        callback(numItems);
+    let queryDoc;
+    if (query.trim() == "") {
+      queryDoc = {};
+    } else {
+      queryDoc = { "$text": { "$search": query } };
     }
+
+    this.db.collection('item').find(queryDoc).count((err, numItems) => {
+      assert.equal(err, null);
+      callback(numItems);
+    });
+  }
 
 
   getItem(itemId, callback) {
